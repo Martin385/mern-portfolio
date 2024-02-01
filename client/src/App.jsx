ba4c6fb505dab331aea46fdcc45827123ba5d268
Martin385 <martomastropietro@gmail.com>
2024-02-01 14:35:26 -0300

    create sign up page ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -17,7 +17,7 @@ export default function App() {
         <Route path="/" element = {<Home></Home>}></Route>
         <Route path="/about" element={<About/>}></Route>
         <Route path="/sign-in" element={<SignIn/>}></Route>
-        <Route path="/sign-in" element={<SignUp/>}></Route>
+        <Route path="/sign-up" element={<SignUp/>}></Route>
         <Route path="/dashboard" element={<Dashboard/>}></Route>
         <Route path="/projects" element={<Projects/>}></Route>
       </Routes>
