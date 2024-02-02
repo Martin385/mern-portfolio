9f176d64d7138e60f29c93949d1420876dcbff48
Martin385 <martomastropietro@gmail.com>
2024-02-02 11:23:40 -0300

    complete use image upload functionlaity

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -1,31 +1,46 @@
-import { useEffect, useState } from 'react'
-import {Sidebar} from 'flowbite-react'
-import {HiArrowSmRight, HiUser} from 'react-icons/hi'
-import {Link, useLocation } from 'react-router-dom'
+import { useEffect, useRef, useState } from "react";
+import { Sidebar } from "flowbite-react";
+import { HiArrowSmRight, HiUser } from "react-icons/hi";
+import { Link, useLocation } from "react-router-dom";
 
 export default function DashSidebar() {
-    const location = useLocation()
-    const [tab, setTab] = useState('')
-    useEffect(() => {
-      const urlParams = new URLSearchParams(location.search)
-      const tabFromUrl = urlParams.get('tab')
-      if (tabFromUrl) {
-        setTab(tabFromUrl)
-      }
-      
-    }, [location.search])
+  const location = useLocation();
+  const [tab, setTab] = useState("");
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const tabFromUrl = urlParams.get("tab");
+    if (tabFromUrl) {
+      setTab(tabFromUrl);
+    }
+  }, [location.search]);
   return (
-    <Sidebar className='w-full md:w-56'> 
-        <Sidebar.Items>
-            <Sidebar.ItemGroup>
-                <Link to ='/dashboard?tab=profile'>
-                <Sidebar.Item active={tab === 'profile'} icon={HiUser} label={"User"} labelColor = 'dark'>Perfil</Sidebar.Item>
-                </Link>
-            </Sidebar.ItemGroup>
-            <Sidebar.ItemGroup>
-                <Sidebar.Item className='cursor-pointer' active icon={HiArrowSmRight} label={""} labelColor = 'dark'>Cerrar sesion</Sidebar.Item>
-            </Sidebar.ItemGroup>
-        </Sidebar.Items>
+    <Sidebar className="w-full md:w-56">
+      <Sidebar.Items>
+        <Sidebar.ItemGroup>
+          <Link to="/dashboard?tab=profile">
+            <Sidebar.Item
+              active={tab === "profile"}
+              icon={HiUser}
+              label={"User"}
+              labelColor="dark"
+              as='div'
+            >
+              Perfil
+            </Sidebar.Item>
+          </Link>
+        </Sidebar.ItemGroup>
+        <Sidebar.ItemGroup>
+          <Sidebar.Item
+            className="cursor-pointer"
+            active
+            icon={HiArrowSmRight}
+            label={""}
+            labelColor="dark"
+          >
+            Cerrar sesion
+          </Sidebar.Item>
+        </Sidebar.ItemGroup>
+      </Sidebar.Items>
     </Sidebar>
-  )
+  );
 }
