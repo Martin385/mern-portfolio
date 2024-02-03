13d5b97fbd29bf42d4c59e6ce8edd4350b6e8331
Martin385 <martomastropietro@gmail.com>
2024-02-03 12:49:18 -0300

    add show more functionality to the posts results of the dashboard

--- client/src/components/DashPost.jsx
+++ client/src/components/DashPost.jsx
@@ -5,6 +5,7 @@ import { Link } from "react-router-dom";
 export default function DashPost() {
   const { currentUser } = useSelector((state) => state.user);
   const [userPosts, setUserPosts] = useState([]);
+  const [showMore, setShowMore] = useState(true)
   useEffect(() => {
     const fetchPosts = async () => {
       try {
@@ -12,6 +13,9 @@ export default function DashPost() {
         const data = await res.json();
         if (res.ok) {
           setUserPosts(data.posts);
+          if (data.posts.length < 9) {
+            setShowMore(false)
+          }
         }
       } catch (error) {
         console.log(error);
@@ -21,6 +25,21 @@ export default function DashPost() {
       fetchPosts();
     }
   }, [currentUser._id]);
+  const handleShowMore = async () => {
+    const startIndex = userPosts.length
+    try {
+      const res = await fetch(`/api/post/getposts?userId=${currentUser._id}&startIndex=${startIndex}`)
+      const data = await res.json()
+      if(res.ok) {
+        setUserPosts((prev) => [...prev, ...data.posts])
+        if (data.posts.length < 9) {
+          setShowMore(false)
+        }
+      }
+    } catch(error) {
+      console.log(error);
+    }
+  }
   return (
     <div className="table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500">
       {currentUser.isAdmin && userPosts.length > 0 ? (
@@ -77,6 +96,13 @@ export default function DashPost() {
               </Table.Body>
             ))}
           </Table>
+          {
+            showMore && (
+              <button onClick={handleShowMore} className="w-full text-teal-500 self-center text-sm py-7">
+                Mostrar mas
+              </button>
+            )
+          }
         </>
       ) : (
         <p>No tenes posts todavia</p>
