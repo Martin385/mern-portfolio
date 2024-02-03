c00580afd876d4bb402e428f2163e845a16ab148
Martin385 <martomastropietro@gmail.com>
2024-02-03 10:47:24 -0300

    show usewr post inside dashboard

--- client/src/components/DashPost.jsx
+++ client/src/components/DashPost.jsx
@@ -1,7 +1,86 @@
-import React from 'react'
-
+import { Table } from "flowbite-react";
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 export default function DashPost() {
+  const { currentUser } = useSelector((state) => state.user);
+  const [userPosts, setUserPosts] = useState([]);
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const res = await fetch(`/api/post/getposts?userId=${currentUser._id}`);
+        const data = await res.json();
+        if (res.ok) {
+          setUserPosts(data.posts);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    if (currentUser.isAdmin) {
+      fetchPosts();
+    }
+  }, [currentUser._id]);
   return (
-    <div>DashPost</div>
-  )
+    <div className="table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500">
+      {currentUser.isAdmin && userPosts.length > 0 ? (
+        <>
+          <Table hoverable className="shadow-md">
+            <Table.Head>
+              <Table.HeadCell>Fecha</Table.HeadCell>
+              <Table.HeadCell>Imagen</Table.HeadCell>
+              <Table.HeadCell>Titulo</Table.HeadCell>
+              <Table.HeadCell>Categoria</Table.HeadCell>
+              <Table.HeadCell>Eliminar</Table.HeadCell>
+              <Table.HeadCell>
+                <span>Editar</span>
+              </Table.HeadCell>
+            </Table.Head>
+            {userPosts.map((post) => (
+              <Table.Body className="divide-y">
+                <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
+                  <Table.Cell>
+                    {new Date(post.updatedAt).toLocaleDateString()}
+                  </Table.Cell>
+                  <Table.Cell>
+                    <Link to={`/post/${post.slug}`}>
+                      <img
+                        src={post.image}
+                        alt={post.title}
+                        className="w-20 h-10 object-cover bg-gray-500"
+                      />
+                    </Link>
+                  </Table.Cell>
+                  <Table.Cell>
+                    <Link
+                      className="font-medium text-gray-500 dark:text-white"
+                      to={`/post/${post.slug}`}
+                    >
+                      {post.title}
+                    </Link>
+                  </Table.Cell>
+                  <Table.Cell>{post.category}</Table.Cell>
+                  <Table.Cell>
+                    <span className="font-medium text-red-500 hover:underline cursor-pointer">
+                      Eliminar
+                    </span>
+                  </Table.Cell>
+                  <Table.Cell>
+                    <Link
+                      className="text-teal-500 hover:underline"
+                      to={`update-post/${post._id}`}
+                    >
+                      <span>Editar</span>
+                    </Link>
+                  </Table.Cell>
+                </Table.Row>
+              </Table.Body>
+            ))}
+          </Table>
+        </>
+      ) : (
+        <p>No tenes posts todavia</p>
+      )}
+    </div>
+  );
 }
--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -13,9 +13,9 @@ export default function FooterComponent() {
               className="self-center whitespace-nowrap text-lg sm:text-xl font-semibold dark:text-white"
             >
               <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
-                Mastros
+                Port
               </span>
-              Blog
+              Flix
             </Link>
           </div>
           <div className="grid gris-cols-2 gap-8  mt-4 sm:grid-cols-3 sm:gap-6">
@@ -34,7 +34,7 @@ export default function FooterComponent() {
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                Mastros blog
+                Port flix
               </Footer.Link>
             </Footer.LinkGroup>
                 </div>
@@ -76,7 +76,7 @@ export default function FooterComponent() {
         </div>
         <Footer.Divider/>
         <div className="w-full sm:flex sm:items-center sm:justify-between">
-            <Footer.Copyright href="#" by="Mastro's Blog" year={new Date().getFullYear()}/>
+            <Footer.Copyright href="#" by="Martin Mastropietro" year={new Date().getFullYear()}/>
 
             <div className="flex gap-6 sm:mt-0 mt-4 sm:justify-center">
                 <Footer.Icon href="#" icon={BsFacebook}/>
